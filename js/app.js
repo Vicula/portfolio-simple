b393c23cc1b20dad306c313d5a17301d7d668198
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-19 09:53:22 -0500

    got the mobile menu to work 'thanks bubba'

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('hey');
+
+var menuToggle = document.querySelector('.navigation__button');
+var menu = document.querySelector('.navigation__menu');
+var onOff = false;
+
+var menuFunc = function(){
+  // console.log(menuToggle.class)
+  if(onOff === false){
+    onOff = true
+    console.log(menuToggle)
+    menuToggle.className = "navigation__button on"
+    menu.className = "navigation__menu"
+    // console.log(menuToggle.class)
+  } else {
+    console.log('tickle')
+    onOff = false
+    menuToggle.className = "navigation__button"
+    menu.className = "navigation__menu u__hidden"
+  }
+}
+
+menuToggle.addEventListener("click", menuFunc);
