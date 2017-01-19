4578c577ee227a17544f31ed69faf4c2dcf40a1d
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-19 10:46:30 -0500

    starting on the skills now

--- js/app.js
+++ js/app.js
@@ -7,12 +7,12 @@ var menuFunc = function(){
   // console.log(menuToggle.class)
   if(onOff === false){
     onOff = true
-    console.log(menuToggle)
+    // console.log(menuToggle)
     menuToggle.className = "navigation__button on"
     menu.className = "navigation__menu"
     // console.log(menuToggle.class)
   } else {
-    console.log('tickle')
+    // console.log('tickle')
     onOff = false
     menuToggle.className = "navigation__button"
     menu.className = "navigation__menu u__hidden"
